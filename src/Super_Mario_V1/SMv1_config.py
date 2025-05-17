993c4dc27c2c0dda4c58006b796547472ab640a1
Bart Kowal <bartkw12@gmail.com>
2025-05-17 18:15:45 -0400

    changed hyperparameters to be more short-term focused and aggressive, improving exploration

--- src/Super_Mario_V1/SMv1_config.py
+++ src/Super_Mario_V1/SMv1_config.py
@@ -2,13 +2,13 @@
 # Hyperparameters
 framestack = 4
 lr = 0.0003               # Increased from 0.000001
-update_steps = 1024       # Reduced from 512
+update_steps = 2048       # Reduced from 512
 total_timesteps = 200000  # Reduced from 1,000,000
 batch_size = 128
 n_epochs = 10
-gamma = 0.9               # Discount factor (prioritize short-term rewards)
+gamma = 0.85              # Discount factor (prioritize short-term rewards)
 gae_lambda = 0.95         # Balance bias/variance in advantage estimation
-clip_range = 0.3          # Standard PPO clipping for policy updates
-ent_coef = 0.03           # Encourage exploration
+clip_range = 0.35          # Standard PPO clipping for policy updates
+ent_coef = 0.04           # Encourage exploration
 
 
