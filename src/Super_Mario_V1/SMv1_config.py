4e9407b97d89962de47ee7456313416c7464a4d3
Bart Kowal <bartkw12@gmail.com>
2025-05-19 15:43:46 -0400

    fixed lr_schedule error in config file, only takes one argument.

--- src/Super_Mario_V1/SMv1_config.py
+++ src/Super_Mario_V1/SMv1_config.py
@@ -4,16 +4,16 @@ from stable_baselines3.common.utils import get_schedule_fn
 # Hyperparameters
 framestack = 4
 
-lr = 2.5e-4               # Increased from 0.000001
-lr_schedule = get_schedule_fn(lr, 'linear')
+lr = 2.5e-4
+lr_schedule = get_schedule_fn(lr)
 
-update_steps = 2048       # Reduced from 512
+update_steps = 2048
 total_timesteps = 1000000
 batch_size = 64
 n_epochs = 8
-gamma = 0.99              # Need long‐term credit for clearing gaps.
-gae_lambda = 0.95         # Balance bias/variance in advantage estimation
-clip_range = 0.1          # Tighter clipping stabilizes training.
+gamma = 0.99               # Need long‐term credit for clearing gaps.
+gae_lambda = 0.95          # Balance bias/variance in advantage estimation
+clip_range = 0.1           # Tighter clipping stabilizes training.
 ent_coef = 0.005           # Encourage exploration w/o chaos
 
 
